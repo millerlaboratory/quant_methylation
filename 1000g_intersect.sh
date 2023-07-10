6a1b5980a7dc353c30f42e4c5aed7f2050b0b93b
Sophia Gibson <73716087+sophia-gibson@users.noreply.github.com>
2023-07-10 14:25:43 -0700

    Add files via upload

---
+++ 1000g_intersect.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+script=/n/users/sgibson/scripts/simplified_intersect.sh
+
+module load samtools/1.12  
+module load bedtools/2.30.0
+module load R/4.2.3
+
+INPUT_DIR=/n/users/sgibson/1000g_methylation
+
+cd $INPUT_DIR
+
+#files=$(find "$INPUT_DIR" -type f -name "*.cpg.bed.gz")
+
+#for file in $files; do
+    #gunzip "$file"
+#done
+
+# Loop through subdirectories
+for subdir in "$INPUT_DIR"/*; do
+  if [ -d "$subdir" ]; then
+    # Run the shell script for each subdirectory
+    cd "$subdir"
+    sh "$script" $subdir
+    
+    echo "Script executed for directory: $subdir"
+  fi
+done
