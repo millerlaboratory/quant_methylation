e685fd2cabd4ecf75c96ba6989f95dcce357df89
Sophia Gibson <sbgibson98@gmail.com>
2023-07-10 15:41:16 -0700

    minor edits

--- scripts/1000g_intersect.sh
+++ scripts/1000g_intersect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#test
+#Script for running the bedtools interect and quantification script on each 1000g sample
 
 script=/n/users/sgibson/scripts/simplified_intersect.sh
 
@@ -12,11 +12,11 @@ INPUT_DIR=/n/users/sgibson/1000g_methylation
 
 cd $INPUT_DIR
 
-#files=$(find "$INPUT_DIR" -type f -name "*.cpg.bed.gz")
+files=$(find "$INPUT_DIR" -type f -name "*.cpg.bed.gz")
 
-#for file in $files; do
-    #gunzip "$file"
-#done
+for file in $files; do
+  #gunzip "$file"
+done
 
 # Loop through subdirectories
 for subdir in "$INPUT_DIR"/*; do
