c76d854df41512afe74e86081e3f21117fed73db
Sophia Gibson <73716087+sophia-gibson@users.noreply.github.com>
2023-07-11 16:56:42 -0700

    Update subdir.sh

--- scripts/subdir.sh
+++ scripts/subdir.sh
@@ -2,13 +2,13 @@
 
 #Script for running the bedtools interect and quantification script on each 1000g sample
 
-script=/n/users/quant_methylation/quant_methylation_modkit.sh
+script=/n/users/sgibson/quant_methylation/scripts/modkit_intersect.sh
 
 module load samtools/1.12  
 module load bedtools/2.30.0
 module load R/4.2.3
 
-INPUT_DIR=/n/users/sgibson/1000g_methylation
+INPUT_DIR=/n/users/sgibson/1000g_methylation/1000g_modkit
 
 cd $INPUT_DIR
 
