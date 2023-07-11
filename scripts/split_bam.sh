6d788e1024775f3877b8129660922e0ffe613ac7
Sophia Gibson <73716087+sophia-gibson@users.noreply.github.com>
2023-07-11 16:54:23 -0700

    Update split_bam.sh

--- scripts/split_bam.sh
+++ scripts/split_bam.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-INPUT_DIR=/n/1000g/align-card
 WORKING_DIR=/n/users/sgibson/1000g_methylation/1000g_modkit
-REF=/n/users/sgibson/reference/GCA_000001405.15_GRCh38_no_alt_analysis_set_maskedGRC_exclusions_v2.fasta
+REFERENCE=/n/users/sgibson/quant_methylation/reference/GCA_000001405.15_GRCh38_no_alt_analysis_set_maskedGRC_exclusions_v2.fasta
+directory_list_file="/n/users/sgibson/1000g_methylation/directory_list.txt"
 
+module load modkit/0.1.3
 
-module load quast
-
-cd $WORKING_DIR
-
-# Find all fasta files within subdirectories
-fasta_files=$(find "$INPUT_DIR" -type f -name "*.haplotagged.bam")
-
-# Iterate over each fasta file
-for file in $fasta_files; do
-samtools view -b -d HP:1 --threads 10 "$file" >"$WORKING_DIR/$NUM.hp1.bam"
-samtools view -b -d HP:2 --threads 10 "$file" >"$WORKING_DIR/$NUM.hp2.bam"
-done
+while IFS= read -r directory_name; do
+#run modkit on each bam file
+    for file in $WORKING_DIR/$directory_name/*.bam; do
+    modkit pileup "$file" --cpg --ref $REFERENCE --ignore h --combine-strands "$WORKING_DIR/$directory_name/${file##*/}.cpg.bed"
+    done
+done < "$directory_list_file"
