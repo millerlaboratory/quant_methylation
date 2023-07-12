14373fdb6b1ed8601331137cd701b831ae590550
Sophia Gibson <sbgibson98@gmail.com>
2023-07-12 13:12:48 -0700

    updates

--- scripts/quant_methylation_modkit.sh
+++ scripts/quant_methylation_modkit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#Update verion of modkit to partition based on HP tag.
+
 WORKING_DIR=/n/users/sgibson/1000g_methylation/1000g_modkit
 REFERENCE=/n/users/sgibson/quant_methylation/reference/GCA_000001405.15_GRCh38_no_alt_analysis_set_maskedGRC_exclusions_v2.fasta
 directory_list_file="/n/users/sgibson/1000g_methylation/directory_list.txt"
@@ -7,8 +9,11 @@ directory_list_file="/n/users/sgibson/1000g_methylation/directory_list.txt"
 module load modkit/0.1.3
 
 while IFS= read -r directory_name; do
+
+mkdir -p "$WORKING_DIR/$directory_name"
+
 #run modkit on each bam file
     for file in $WORKING_DIR/$directory_name/*.bam; do
-    modkit pileup "$file" --cpg --ref $REFERENCE --ignore h --combine-strands "$WORKING_DIR/$directory_name/${file##*/}.cpg.bed"
+    modkit pileup "$file" --cpg --ref $REFERENCE --ignore h --combine-strands --partition-tag HP "$WORKING_DIR/$directory_name/${file##*/}.cpg.bed"
     done
 done < "$directory_list_file"
