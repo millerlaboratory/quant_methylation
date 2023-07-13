85a42cd1164b8707e22424adaf83f961248f9649
Sophia Gibson <sbgibson98@gmail.com>
2023-07-13 11:28:50 -0700

    updates to script

--- scripts/quant_methylation_modkit.sh
+++ scripts/quant_methylation_modkit.sh
@@ -4,17 +4,16 @@
 
 INPUT_DIR=/n/1000g/align-card-2.24-hg38
 WORKING_DIR=/n/users/sgibson/1000g_methylation/1000g_modkit/v0.1.11
-REFERENCE=/n/users/sgibson/quant_methylation/reference/GCA_000001405.15_GRCh38_no_alt_analysis_set_maskedGRC_exclusions_v2.fasta
+REFERENCE=/n/users/sgibson/reference/GCA_000001405.15_GRCh38_no_alt_analysis_set_maskedGRC_exclusions_v2.fasta
 
 module load modkit/0.1.11
 
 cd $WORKING_DIR
 
 #list all of the bam files
-files=$(find $INPUT_DIR -type f -name "*_PMDV_FINAL_haplotagged.bam")
+files=$(find $INPUT_DIR -type f -name "*_PMDV_FINAL.haplotagged.bam")
 
 #run modkit on each bam file
 for file in $files; do
-    modkit pileup "$file" --cpg --ref $REFERENCE --ignore h --combine-strands --partition-tag HP "$WORKING_DIR/${file##*/}" --log-filepath "$WORKING_DIR/${file##*/}/pileup.log" \
-     --prefix "${file##*/}.cpg"
+    modkit pileup "$file" --cpg --ref $REFERENCE --ignore h --combine-strands --partition-tag HP "$WORKING_DIR/${file##*/}" --log-filepath "$WORKING_DIR/${file##*/}/pileup.log" --prefix "${file##*/}.cpg"
 done
