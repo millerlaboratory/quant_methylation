903c87a8517deb8102427c0e58b059f19a66bc1c
Sophia Gibson <sbgibson98@gmail.com>
2023-07-13 11:16:43 -0700

    update modkit script

--- scripts/quant_methylation_modkit.sh
+++ scripts/quant_methylation_modkit.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-#Update verion of modkit to partition based on HP tag.
+#Updated verion of modkit to partition based on HP tag.
 
-WORKING_DIR=/n/users/sgibson/1000g_methylation/1000g_modkit
+INPUT_DIR=/n/1000g/align-card-2.24-hg38
+WORKING_DIR=/n/users/sgibson/1000g_methylation/1000g_modkit/v0.1.11
 REFERENCE=/n/users/sgibson/quant_methylation/reference/GCA_000001405.15_GRCh38_no_alt_analysis_set_maskedGRC_exclusions_v2.fasta
-directory_list_file="/n/users/sgibson/1000g_methylation/directory_list.txt"
 
-module load modkit/0.1.3
+module load modkit/0.1.11
 
-while IFS= read -r directory_name; do
+cd $WORKING_DIR
 
-mkdir -p "$WORKING_DIR/$directory_name"
+#list all of the bam files
+files=$(find $INPUT_DIR -type f -name "*_PMDV_FINAL_haplotagged.bam")
 
 #run modkit on each bam file
-    for file in $WORKING_DIR/$directory_name/*.bam; do
-    modkit pileup "$file" --cpg --ref $REFERENCE --ignore h --combine-strands --partition-tag HP "$WORKING_DIR/$directory_name/${file##*/}.cpg.bed"
-    done
-done < "$directory_list_file"
+for file in $files; do
+    modkit pileup "$file" --cpg --ref $REFERENCE --ignore h --combine-strands --partition-tag HP "$WORKING_DIR/${file##*/}" --log-filepath "$WORKING_DIR/${file##*/}/pileup.log" \
+     --prefix "${file##*/}.cpg"
+done
