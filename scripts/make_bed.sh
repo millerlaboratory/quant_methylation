86f3fcdfd1521ed2f4b27168e89e2cffa98c586d
Sophia Gibson <sbgibson98@gmail.com>
2023-08-17 12:11:46 -0700

    practice edit

--- scripts/make_bed.sh
+++ scripts/make_bed.sh
@@ -4,6 +4,8 @@ WORKING_DIR=/n/users/sgibson/1000g_methylation/1000g_modkit/modkit_v0.1.11/DSS_D
 
 cd $WORKING_DIR
 
+module load R
+
 for file in $WORKING_DIR/*.tsv; do
 cat $file | awk '{print $1 "\t" $2 "\t" $3}' | tail -n +2 > $WORKING_DIR/${file##*/}.bed
 done
